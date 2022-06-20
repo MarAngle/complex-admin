0b949f4b53b13f896da6fec56828a40bc2637f4c
MarAngle <yinweiyingzhe@126.com>
2022-06-20 08:32:30 +0800

    准备添加less

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
+let productionSourceMap = true
+if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_APITYPE === 'prod') {
+  productionSourceMap = false
+}
+
 module.exports = defineConfig({
+  productionSourceMap: productionSourceMap,
   transpileDependencies: true
 })
